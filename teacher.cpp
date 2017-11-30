6f9520d45aa50ebc6142ca5446f98b075c03b07c
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2017-11-30 15:42:17 +0300

    teacher

--- teacher.cpp
+++ teacher.cpp
@@ -1,6 +1,59 @@
 #include "teacher.h"
+#include <iostream>
 
 teacher::teacher()
 {
 
+}
+
+teacher::teacher(std::string fName, std::string lName, unsigned char ageSt)
+{
+    teacher::firstName = fName;
+    teacher::lastName = lName;
+    teacher::age = ageSt;
+}
+
+teacher::~teacher()
+{
+
+}
+
+std::string teacher::getFirstName() const
+{
+    return firstName;
+}
+
+void teacher::SetFirstName(std::string fName)
+{
+    this->firstName = fName;
+}
+
+std::string teacher::getLastName() const
+{
+    return lastName;
+}
+
+void teacher::SetLastName(std::string lName)
+{
+    this -> lastName = lName;
+}
+
+char teacher::getAge() const
+{
+    return age;
+}
+
+void teacher::SetAge(unsigned char ageSt)
+{
+    this -> age = ageSt;
+}
+
+void teacher::GradeStudent()
+{
+    std::cout << "\n" << "Student graded" << std::endl;
+}
+
+void teacher::SitInClass()
+{
+    std::cout << "\n" << "Sitting in front of class" << std::endl;
 }
--- teacher.h
+++ teacher.h
@@ -1,11 +1,30 @@
 #ifndef TEACHER_H
 #define TEACHER_H
-
+#pragma once
+#include <string>
 
 class teacher
 {
 public:
     teacher();
+    teacher(std::string fName, std::string lName, unsigned __int8 ageSt);
+    ~teacher();
+
+    std::string getFirstName() const;
+    void SetFirstName(std::string fName);
+    std::string getLastName() const;
+    void SetLastName(std::string lName);
+    __int8 getAge() const;
+    void SetAge(unsigned __int8 ageSt);
+
+    void GradeStudent();
+    void SitInClass();
+
+
+private:
+    std::string firstName;
+    std::string lastName;
+    unsigned __int8 age;
 };
 
 #endif // TEACHER_H
