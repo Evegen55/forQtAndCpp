00326b722ae150e20ebbf1fa2f46cf03497e4359
Evgenii_Lartcev <Evgenii_Lartcev@epam.com>
2018-01-31 15:13:05 +0300

    doc

--- different_functions.h
+++ different_functions.h
@@ -9,7 +9,17 @@ void passByValue(int num1);
 void passByRef(int &num1);
 void allocatingMemory();
 
+/**
+ * @brief saxpy uses __restrict it means it works correctly with MSVC compiler only.
+ * In MinGW it can't work.
+ *
+ * @param n
+ * @param a
+ * @param x
+ * @param y
+ */
 void saxpy(int n, float a, float * __restrict x, float * __restrict y);
+
 void doSaxpy();
 void doSaxpyAndMeasureTime();
 
